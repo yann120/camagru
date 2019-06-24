94b7e0060fc6e15c2c728810d0a6289f41d503a7
Yann <yann120@hotmail.fr>
2019-06-24 12:06:04 +0200

    remove old HTML

--- user/sign_up.php
+++ user/sign_up.php
@@ -18,29 +18,21 @@
 			<div class="field">
 			<label class="label">Username</label>
 			<div class="control has-icons-left has-icons-right">
-				<input class="input is-success" type="text" name="username" required placeholder="Username">
+				<input class="input" type="text" name="username" required placeholder="Username">
 				<span class="icon is-small is-left">
 				<i class="fas fa-user"></i>
 				</span>
-				<span class="icon is-small is-right">
-				<i class="fas fa-check"></i>
-				</span>
 			</div>
-			<p class="help is-success">This username is available</p>
 			</div>
 
 			<div class="field">
 			<label class="label">Email</label>
 			<div class="control has-icons-left has-icons-right">
-				<input class="input is-danger" type="email" name="email" required placeholder="Email">
+				<input class="input" type="email" name="email" required placeholder="Email">
 				<span class="icon is-small is-left">
 				<i class="fas fa-envelope"></i>
 				</span>
-				<span class="icon is-small is-right">
-				<i class="fas fa-exclamation-triangle"></i>
-				</span>
 			</div>
-			<p class="help is-danger">This email is invalid</p>
 			</div>
 
 			<div class="field">
