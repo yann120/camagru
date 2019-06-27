c966a11692f0cd3fc3808067a793fe36a8f69bf8
Yann PETITJEAN <yann120@hotmail.fr>
2019-06-27 12:28:43 +0200

    Font Awesome CDN in JS replaced by CDN in CSS

--- user/sign_up.php
+++ user/sign_up.php
@@ -8,7 +8,7 @@
         <title>Camagru</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
-        <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css">
     </head>
     <body>
 		<h1 class="title is-1 has-text-centered">Sign up</h1>
